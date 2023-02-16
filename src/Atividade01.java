4bc0f686aaccff0f8d603c43f81c2535896bce49
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-02-16 08:21:58 -0300

    feat: 150223

---
+++ src/Atividade01.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+/* Atividade
+ * 1. Leia um nuemro real equivalente a polegadas e converta para centímetros (1 polegada = 2,54 cm)
+ */
+public class Atividade01 {
+    public static void main(String[] args) {
+        double polegadas, centimetros;
+        polegadas = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor em polegadas: "));
+        centimetros = polegadas * 2.54;
+        JOptionPane.showMessageDialog(null, polegadas + " polegadas equivalem a " + centimetros + " centímetros");
+    }
+    
+}
